1209a5685aa9c44ce1b5a517fcf2b3fef231f8e6
Jayadeep KM <kmjayadeep@gmail.com>
2016-05-20 18:20:36 +0530

    removed val

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -175,23 +175,13 @@ angular.module('frontApp')
                 }
             },
             "fc": {
-                "name": "Fixed Commission on turnover",
+                "name": "Variable income based on sales",
                 "input": true,
                 "value": 0,
                 "slider": {
                     "min": 0,
                     "max": 1000000,
                     "step": 1000
-                }
-            },
-            evi: {
-                name: "Estimated Variable Income",
-                value: 0,
-                input: true,
-                slider: {
-                    min: 0,
-                    max: 10000000,
-                    step: 1000
                 }
             },
             "c2": {
@@ -334,7 +324,7 @@ angular.module('frontApp')
             if (nohra)
                 return
             $scope.variables.c3.value = $scope.variables.da.value + $scope.variables.fc.value
-            $scope.variables.c2.value = $scope.variables.ma.value + $scope.variables.lta.value + $scope.variables.cea.value + $scope.variables.ca.value + $scope.variables.meal.value + $scope.variables.evi.value;
+            $scope.variables.c2.value = $scope.variables.ma.value + $scope.variables.lta.value + $scope.variables.cea.value + $scope.variables.ca.value + $scope.variables.meal.value;
             var res = optimise(checkboxValue);
             if (which == undefined || which & 1) {
                 $scope.variables.x1.slider.max = Math.round(res[0])
@@ -539,9 +529,8 @@ angular.module('frontApp')
                 "HRA amount : " + ($scope.variables.residence.value != "Owned" ? ($scope.variables.x2.value - $scope.variables.z.value) : 0),
                 "Vacation travelling Sponsorship (LTA) : " + Math.max($scope.variables.lta.value - $scope.variables.lta.slider.max, 0),
                 "DA amount : " + $scope.variables.da.value,
-                "FC amount : " + $scope.variables.fc.value,
+                "Variable income on sales : " + $scope.variables.fc.value,
                 "CA amount : " + Math.max(0, $scope.variables.ca.value - 19200),
-                "Estimated Variable Income : " + $scope.variables.evi.value,
                 "Others : " + $scope.variables.c4.value
             ];
 
@@ -560,7 +549,6 @@ angular.module('frontApp')
                 $scope.variables.da.value +
                 $scope.variables.fc.value +
                 (0 /*$scope.variables.ca.value - 19200*/ ) +
-                $scope.variables.evi.value +
                 $scope.variables.c4.value);
             if ($scope.variables.pfesi.checkbox.value && !$scope.variables.pfesi.hide) {
                 var m = ["PF amount : " + (0 /*-$scope.variables.pfesi.pfvalue*/ ),
